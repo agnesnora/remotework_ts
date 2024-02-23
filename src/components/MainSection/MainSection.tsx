d40619286652ee696ff390cc7d9122c2931bde92
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2024-02-23 18:27:13 +0100

    styling main page

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -4,14 +4,16 @@ export const MainSection = () => {
   return (
     <div className="hero">
       <img className="hero--img" src="../../../public/image-hero-mobile.png" />
-      <h1>Make remote work</h1>
-      <p>
-        Get your team in sync, no matter your location. Streamline processes,
-        create team rituals, and watch productivity soar.
-      </p>
-      <Button className="main--btn" onClick={() => console.log("learn more")}>
-        Learn more
-      </Button>
+      <div className="hero--content">
+        <h1>Make remote work</h1>
+        <p>
+          Get your team in sync, no matter your location. Streamline processes,
+          create team rituals, and watch productivity soar.
+        </p>
+        <Button className="main--btn" onClick={() => console.log("learn more")}>
+          Learn more
+        </Button>
+      </div>
     </div>
   );
 };
