f66178b839d35ceb7618fe86de78f69ee1f1d39a
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2024-02-23 09:11:05 +0100

    testing navbar

--- src/__test__/Navbar.test.tsx
+++ src/__test__/Navbar.test.tsx
@@ -1,9 +1,15 @@
 import { Navbar } from "../components/Navbar/Navbar";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 describe("Navbar renders", () => {
   test("Navbar renders on page", () => {
     render(<Navbar />);
     expect(true).toBeTruthy();
+  });
+
+  test("Navbar renders on page", () => {
+    render(<Navbar />);
+    expect(screen.getByText("Features")).toBeInTheDocument();
   });
 });
